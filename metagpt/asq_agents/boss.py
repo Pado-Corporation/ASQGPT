d683a7e8ed39ebc633aee8acdaff839eec495119
ComputelessComputer <jeeheontransformers@gmail.com>
2023-10-24 20:36:48 +0900

    New agents

--- metagpt/asq_agents/boss.py
+++ metagpt/asq_agents/boss.py
@@ -2,20 +2,23 @@ import os
 import openai
 import sys
 from functions import read_file, check_completion
-from metagpt.config import CONFIG
 
-
-openai.api_key = CONFIG.openai_api_key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class Boss:
     def __init__(self, model_type="gpt-4"):
         # Reads the boss' prompt.
-        boss_prompt = read_file("boss_prompt.md")
+        instruction = read_file("boss.md")
         # Sets the model that will be powering the boss.
         self.model_type = model_type
         # Initializes the first context to be delivered to the language model.
-        self.messages = [{"role": "system", "content": boss_prompt}]
+        self.messages = [
+            {
+                "role": "system",
+                "content": instruction
+            }
+        ]
 
     def greetings(self):
         welcome_text = read_file("welcome.txt")
@@ -31,7 +34,12 @@ class Boss:
             if not message:
                 self.listen()
             else:
-                self.messages.append({"role": "user", "content": message})
+                self.messages.append(
+                    {
+                        "role": "user",
+                        "content": message
+                    }
+                )
         except KeyboardInterrupt:
             print("\nASQ: Good talk! I'll see you later :)")
             sys.exit(1)
@@ -39,7 +47,7 @@ class Boss:
     def speak(self):
         """
         The agent responds to the user's message.
-        :return:
+        :return: True if B0$$ has reached the goal.
         """
         # Receive a chat completion object from the OpenAI API.
         response = openai.ChatCompletion.create(
@@ -48,24 +56,26 @@ class Boss:
             temperature=0,
             top_p=1,
             frequency_penalty=0,
-            presence_penalty=0,
+            presence_penalty=0
         )
         response_message = response["choices"][0]["message"]
+        msg = response_message["content"]
         # Show the boss' response to the user.
-        print(f'ASQ: {response_message["content"]}')
-        check_completion("[GOAL]", response_message["content"])
-        # Add the boss' message to the context.
+        print(f'ASQ: {msg}')
         self.messages.append(response_message)
+
+        return check_completion("[GOAL]", msg)
 
     def conversation(self):
         """
         The agent has a conversation with the user.
         :return:
         """
-        self.listen()
-        print("---")
-        self.speak()
-        self.conversation()
+        finished = False
+        while not finished:
+            self.listen()
+            print("---")
+            finished = self.speak()
 
     def run(self):
         """
--- metagpt/asq_agents/functions.py
+++ metagpt/asq_agents/functions.py
@@ -1,4 +1,8 @@
-import sys
+import os
+
+import requests
+
+supabase_key = os.getenv("ASQ_DB_KEY")
 
 
 def read_file(filename):
@@ -14,12 +18,17 @@ def read_file(filename):
 
 def check_completion(tag, message):
     """
-    Check if the agent has completed its role, by searching for a "tag". If true, finish the process.
+    Check if the agent has completed its role, by searching for a "tag".
     :param tag: Indicator that the task has been completed
     :type tag: str
     :param message: Content that needs to be checked
     :type message: str
     :return:
     """
-    if tag in message:
-        sys.exit()
+    return tag in message
+
+def get_report(id):
+    url = 'https://crhertcomzmmxpsmqozi.supabase.co/rest/v1/reports?id=eq.1&select=*'
+    payload = {'id': id, 'select': 'report_type'}
+    headers = {'apikey': supabase_key, 'Authorization': f'Bearer {supabase_key}', 'Range': '0-9'}
+    response = requests.get(url, params=payload, headers=headers)
